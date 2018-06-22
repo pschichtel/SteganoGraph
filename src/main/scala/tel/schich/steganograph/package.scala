58955900e78ca13c073db3b378ad164ca0bb73cf
Phillip Schichtel <phillip@schich.tel>
2018-06-22 23:42:36 +0200

    even nicer

--- src/main/scala/tel/schich/steganograph/package.scala
+++ src/main/scala/tel/schich/steganograph/package.scala
@@ -1,14 +1,18 @@
 package tel.schich
 
 import java.awt.image.BufferedImage
+import java.io.OutputStream
+import java.nio.file.Files.{newInputStream, newOutputStream}
+import java.nio.file.Paths
 import java.nio.file.StandardOpenOption.{CREATE, READ, TRUNCATE_EXISTING, WRITE}
-import java.nio.file.{Files, Paths}
 
 import javax.imageio.ImageIO
 
 package object steganograph {
 
     case class ARGB(a: Int, r: Int, g: Int, b: Int)
+
+    val OutputFormat = "PNG"
 
     private val bitsPerPixel = 8
     private val secretBitCount = 1
@@ -19,38 +23,22 @@ package object steganograph {
     def crossover(pub: Int, sec: Int): Int =
         (pub & publicBits) | (sec >>> publicBitCount)
 
-    def split(pixChan: Int): (Int, Int) = {
-        val pub = pixChan & publicBits
-        val sec = (pixChan << publicBitCount) & 0xFF
-        (pub, sec)
-    }
+    def split(pixChan: Int): (Int, Int) =
+        (pixChan & publicBits, (pixChan << publicBitCount) & 0xFF)
 
-    def readImg(path: String): BufferedImage = {
-        val in = Files.newInputStream(Paths.get(path), READ)
-        try {
-            ImageIO.read(in)
-        } finally {
-            in.close()
-        }
-    }
+    def loan[A <: AutoCloseable, B](a: A)(f: A => B): B =
+        try f()
+        finally a.close()
 
-    def writeImg(img: BufferedImage, path: String): Boolean = {
-        val out = Files.newOutputStream(Paths.get(path), TRUNCATE_EXISTING, CREATE, WRITE)
-        try {
-            ImageIO.write(img, "PNG", out)
-        } finally {
-            out.close()
-        }
-    }
+    def readImg(path: String): BufferedImage =
+        loan(newInputStream(Paths.get(path), READ))(ImageIO.read)
+
+    def writeImg(img: BufferedImage, path: String, format: String = OutputFormat): Boolean =
+        loan(newOutputStream(Paths.get(path), TRUNCATE_EXISTING, CREATE, WRITE))(ImageIO.write(img, format, _: OutputStream))
 
 
-    def extractChannels(argb: Int): ARGB = {
-        val a = (argb >>> 24) & 0xFF
-        val r = (argb >>> 16) & 0xFF
-        val g = (argb >>> 8) & 0xFF
-        val b = argb & 0xFF
-        ARGB(a, r, g, b)
-    }
+    def extractChannels(argb: Int): ARGB =
+        ARGB((argb >>> 24) & 0xFF, (argb >>> 16) & 0xFF, (argb >>> 8) & 0xFF, argb & 0xFF)
 
     def composeChannels(a: Int, r: Int, g: Int, b: Int): Int =
         ((a & 0xFF) << 24) | ((r & 0xFF) << 16) | ((g & 0xFF) << 8) | (b & 0xFF)
