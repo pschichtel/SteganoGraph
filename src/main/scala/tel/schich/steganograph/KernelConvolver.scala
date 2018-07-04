ed4df4c370504cacf5b47d864eebc407ab7571f5
Phillip Schichtel <phillip@schich.tel>
2018-07-04 20:05:59 +0200

    continue the image around the edges and simplify a lot

--- src/main/scala/tel/schich/steganograph/KernelConvolver.scala
+++ src/main/scala/tel/schich/steganograph/KernelConvolver.scala
@@ -2,98 +2,149 @@ package tel.schich.steganograph
 
 import java.awt.image.BufferedImage
 
+import scala.math.Numeric.Implicits.infixNumericOps
+
 object KernelConvolver {
 
+    def ignore(pixels: Array[ARGB], width: Int, height: Int, x: Int, y: Int): ARGB = ARGB.zero
+
+    def wrap(pixels: Array[ARGB], width: Int, height: Int, x: Int, y: Int): ARGB =
+        pixels((y % height) * width + (x % width))
+
+    def mirror(pixels: Array[ARGB], width: Int, height: Int, x: Int, y: Int): ARGB = {
+        def edgeMirror(n: Int, max: Int) =
+            if (n < 0) n.abs % max
+            else if (n >= max) (max - 1) - (n % max)
+            else n
+
+        pixels(edgeMirror(y, height) * width + edgeMirror(x, width))
+    }
+
+    def extend(pixels: Array[ARGB], width: Int, height: Int, x: Int, y: Int): ARGB = {
+        def snap(n: Int, max: Int) =
+            if (n < 0) 0
+            else if (n >= max) max - 1
+            else n
+
+        pixels(snap(y, height) * width + snap(x, width))
+    }
+
+    type Filter = (Int, Int, Array[ARGB], Continuation) => Array[ARGB]
+    type Continuation = (Array[ARGB], Int, Int, Int, Int) => ARGB
+
     def main(args: Array[String]): Unit = {
-        val filters: Array[(Int, Int, Array[ARGB]) => Array[ARGB]] = args.drop(2).map(_.trim.toLowerCase).map {
-            case "dither" => dither(closest(Seq(ARGB.zero, ARGB.A, ARGB.R, ARGB.G, ARGB.B, ARGB.RG, ARGB.RB, ARGB.GB, ARGB.one))) _
-            case "alpha" => channel(ARGB.A) _
-            case "red" => channel(ARGB.R) _
-            case "green" => channel(ARGB.G) _
-            case "blue" => channel(ARGB.B) _
-            case "red_green" => channel(ARGB.RG) _
-            case "red_blue" => channel(ARGB.RB) _
-            case "green_blue" => channel(ARGB.GB) _
-            case "grayscale" => perPixel(grayscale) _
-            case "sepia" => perPixel(sepia) _
-            case "identity" => kernel(
-                0, 0, 0,
-                0, 1, 0,
-                0, 0, 0
-            ) _
-            case "mean3" => kernel(
-                1, 1, 1,
-                1, 1, 1,
-                1, 1, 1
-            ) _
-            case "mean" => kernel(
-                1, 1, 1, 1, 1,
-                1, 1, 1, 1, 1,
-                1, 1, 1, 1, 1,
-                1, 1, 1, 1, 1,
-                1, 1, 1, 1, 1
-            ) _
-            case "emboss" => kernel(
-                -2, -1, 0,
-                -1,  1, 1,
-                 0,  1, 2
-            ) _
-            case "gauss3" => kernel(
-                1, 2, 1,
-                2, 4, 2,
-                1, 2, 1
-            ) _
-            case "gauss" => kernel(
-                1,  4,  6,  4, 1,
-                1, 16, 24, 16, 4,
-                1, 24, 36, 24, 6,
-                1, 16, 24, 16, 4,
-                1,  4,  6,  4, 1
-            ) _
-            case "edge" => kernel(
-                -1, -1, -1,
-                -1,  8, -1,
-                -1, -1, -1
-            ) _
-            case "edge_x" => kernel(
-                -1, 0, 1,
-                -1, 0, 1,
-                -1, 0, 1
-            ) _
-            case "edge_y" => kernel(
-                -1, -1, -1,
-                 0,  0, 0,
-                 1,  1,  1
-            ) _
-            case "sharpen" => kernel(
-                 0, -1,  0,
-                -1,  5, -1,
-                 0, -1,  0
-            ) _
+        val filters: Array[(Filter, Continuation)] = args.drop(2).map(_.trim.toLowerCase).map { filterSpec =>
+            val (filterName, contName) = filterSpec.split(':') match {
+                case Array(f, c) => (f, c)
+                case Array(f) => (f, "ignore")
+            }
+            val filter: Filter = filterName match {
+                case "dither" => dither(closest(Seq(ARGB.zero, ARGB.A, ARGB.R, ARGB.G, ARGB.B, ARGB.R + ARGB.G, ARGB.R + ARGB.B, ARGB.G + ARGB.B, ARGB.one)))
+                case "alpha" => perPixel(multiply(ARGB.A))
+                case "red" => perPixel(multiply(ARGB.R))
+                case "green" => perPixel(multiply(ARGB.G))
+                case "blue" => perPixel(multiply(ARGB.B))
+                case "red_green" => perPixel(multiply(ARGB.R + ARGB.G))
+                case "red_blue" => perPixel(multiply(ARGB.R + ARGB.B))
+                case "green_blue" => perPixel(multiply(ARGB.G + ARGB.B))
+                case "grayscale" => perPixel(grayscale)
+                case "sepia" => perPixel(sepia)
+                case "identity" => kernel(
+                    0, 0, 0,
+                    0, 1, 0,
+                    0, 0, 0
+                )
+                case "mean3" => kernel(
+                    1, 1, 1,
+                    1, 1, 1,
+                    1, 1, 1
+                )
+                case "mean" => kernel(
+                    1, 1, 1, 1, 1,
+                    1, 1, 1, 1, 1,
+                    1, 1, 1, 1, 1,
+                    1, 1, 1, 1, 1,
+                    1, 1, 1, 1, 1
+                )
+                case "emboss" => kernel(
+                    -2, -1, 0,
+                    -1, 1, 1,
+                    0, 1, 2
+                )
+                case "gauss3" => kernel(
+                    1, 2, 1,
+                    2, 4, 2,
+                    1, 2, 1
+                )
+                case "gauss" => kernel(
+                    1, 4, 6, 4, 1,
+                    1, 16, 24, 16, 4,
+                    1, 24, 36, 24, 6,
+                    1, 16, 24, 16, 4,
+                    1, 4, 6, 4, 1
+                )
+                case "edge" => kernel(
+                    -1, -1, -1,
+                    -1, 8, -1,
+                    -1, -1, -1
+                )
+                case "edge_x" => kernel(
+                    -1, 0, 1,
+                    -1, 0, 1,
+                    -1, 0, 1
+                )
+                case "edge_y" => kernel(
+                    -1, -1, -1,
+                    0, 0, 0,
+                    1, 1, 1
+                )
+                case "sharpen" => kernel(
+                    0, -1, 0,
+                    -1, 5, -1,
+                    0, -1, 0
+                )
+                case _ => throw new IllegalArgumentException("Meh!")
+            }
+
+            val cont: Continuation = contName match {
+                case "ignore" => ignore
+                case "mirror" => mirror
+                case "extend" => extend
+                case _ => throw new IllegalArgumentException("Meh!")
+            }
+
+            (filter, cont)
         }
+
 
         val img = readImg(args(0))
         val width = img.getWidth
         val height = img.getHeight
-        val pixels = (for {
+        val inputPixels = (for {
             y <- 0 until height
             x <- 0 until width
         } yield extractChannels(img.getRGB(x, y))).toArray
 
-        val processed = filters.foldLeft(pixels)((pixels, filter) => filter(width, height, pixels))
+        val processed = filters.foldLeft(inputPixels) {
+            case (pixels, (filter, cont)) => filter(width, height, pixels, cont)
+        }
 
         val output = new BufferedImage(width, height, img.getType)
         for {
             imgY <- 0 until height
             imgX <- 0 until width
         } {
-            output.setRGB(imgX, imgY, processed(imgY * width + imgX).toARGB32)
+            output.setRGB(imgX, imgY, processed(imgY * width + imgX).toInt)
         }
 
         writeImg(output, args(1))
     }
 
-    def grayscale(p: ARGB): ARGB = p.toGray
+
+    def grayscale(p: ARGB): ARGB = {
+        val avg = (p.r + p.g + p.b) / 3.0
+        ARGB(p.a, avg, avg, avg)
+    }
 
     def sepia(p: ARGB): ARGB = ARGB(
         p.a,
@@ -102,10 +153,10 @@ object KernelConvolver {
         math.min(1, (p.r * .272) + (p.g *.534) + (p.b * .131))
     )
 
-    def perPixel(mapColor: ARGB => ARGB)(width: Int, height: Int, pixels: Array[ARGB]): Array[ARGB] =
+    def perPixel(mapColor: ARGB => ARGB)(width: Int, height: Int, pixels: Array[ARGB], cont: Continuation): Array[ARGB] =
         pixels.map(mapColor)
 
-    def dither(mapColor: ARGB => ARGB)(width: Int, height: Int, pixels: Array[ARGB]): Array[ARGB] = {
+    def dither(mapColor: ARGB => ARGB)(width: Int, height: Int, pixels: Array[ARGB], cont: Continuation): Array[ARGB] = {
         val out = Array.ofDim[ARGB](pixels.length)
         Array.copy(pixels, 0, out, 0, pixels.length)
 
@@ -114,7 +165,7 @@ object KernelConvolver {
         def update(x: Int, y: Int, err: ARGB): Unit = {
             val i = idx(x, y)
             if (out.isDefinedAt(i)) {
-                out(i) = out(i) add err
+                out(i) = out(i) + err
             }
         }
 
@@ -128,10 +179,10 @@ object KernelConvolver {
             val err = pixel - newPixel
 
             out(i) = newPixel
-            update(x + 1, y    , err * 7.0/16.0)
-            update(x - 1, y + 1, err * 3.0/16.0)
-            update(x    , y + 1, err * 5.0/16.0)
-            update(x + 1, y + 1, err * 1.0/16.0)
+            update(x + 1, y    , err * (7.0/16.0))
+            update(x - 1, y + 1, err * (3.0/16.0))
+            update(x    , y + 1, err * (5.0/16.0))
+            update(x + 1, y + 1, err * (1.0/16.0))
         }
 
         out
@@ -140,13 +191,12 @@ object KernelConvolver {
     def closest(palette: Seq[ARGB])(search: ARGB): ARGB =
         palette.minBy(_.distanceSquared(search))
 
-    def channel(c: ARGB)(width: Int, height: Int, pixels: Array[ARGB]): Array[ARGB] =
-        pixels.map(_ * c)
+    def multiply(c: ARGB)(p: ARGB): ARGB = p * c
 
-    def kernel(kernel: Double*)(width: Int, height: Int, pixels: Array[ARGB]): Array[ARGB] =
-        applyKernel(width, height, pixels, kernel.toArray)
+    def kernel(kernel: Double*)(width: Int, height: Int, pixels: Array[ARGB], cont: Continuation): Array[ARGB] =
+        applyKernel(width, height, pixels, cont, kernel.toArray)
 
-    def applyKernel(width: Int, height: Int, pixels: Array[ARGB], kernel: Array[Double]): Array[ARGB] = {
+    def applyKernel(width: Int, height: Int, pixels: Array[ARGB], cont: Continuation, kernel: Array[Double]): Array[ARGB] = {
         val kernelSize = math.sqrt(kernel.length).toInt
         val kernelRadius = kernelSize / 2 // integer division to round down
         val kernelOffsets = (for {
@@ -166,7 +216,7 @@ object KernelConvolver {
 
                 if (actualX >= 0 && actualX < width && actualY >= 0 && actualY < height) {
                     pixels(actualY * width + actualX) * w
-                } else ARGB.zero
+                } else cont(pixels, width, height, actualX, actualY) * w
             }).sum
         }).toArray
 
@@ -174,7 +224,7 @@ object KernelConvolver {
         val max = processed.foldLeft(ARGB.MinValue)(_.componentMax(_))
         val diff = max - min
 
-        processed.map(_.lerp(diff))
+        processed.map(_ / diff)
     }
 
 }
--- src/main/scala/tel/schich/steganograph/package.scala
+++ src/main/scala/tel/schich/steganograph/package.scala
@@ -11,44 +11,18 @@ import javax.imageio.ImageIO
 package object steganograph {
 
     case class ARGB(a: Double, r: Double, g: Double, b: Double) {
-        def scale(w: Double) = ARGB(a * w, r * w, g * w, b * w)
-        @inline
-        def * (w: Double): ARGB = scale(w)
-        @inline
-        def / (w: Double): ARGB = scale(1/w)
-
-        def add(other: ARGB): ARGB = ARGB.plus(this, other)
-
-        def toARGB32: Int = composeChannels(a, r, g, b)
-
-        def toGray: ARGB = {
-            val avg = (r + g + b) / 3
-            ARGB(1, avg, avg, avg)
-        }
+        def * (w: Double): ARGB = ARGB(a * w, r * w, g * w, b * w)
 
         def componentMin(other: ARGB) = ARGB(math.min(a, other.a), math.min(r, other.r), math.min(g, other.g), math.min(b, other.b))
-
         def componentMax(other: ARGB) = ARGB(math.max(a, other.a), math.max(r, other.r), math.max(g, other.g), math.max(b, other.b))
 
+        def magnitudeSquared: Double = a * a + r * r + g * g + b * b
         def distanceSquared(other: ARGB): Double = math.pow(a - other.a, 2) + math.pow(r - other.r, 2) + math.pow(g - other.g, 2) + math.pow(b - other.b, 2)
-
-        def lerp(min: ARGB, max: ARGB): ARGB =
-            lerp(max - min)
-
-        def lerp(diff: ARGB): ARGB = ARGB(
-            a / diff.a,
-            r / diff.r,
-            g / diff.g,
-            b / diff.b
-        )
     }
     implicit object ARGB extends Integral[ARGB] {
         val A: ARGB = ARGB(1, 0, 0, 0)
         val R: ARGB = ARGB(0, 1, 0, 0)
-        val RG: ARGB = ARGB(0, 1, 1, 0)
-        val RB: ARGB = ARGB(0, 1, 0, 1)
         val G: ARGB = ARGB(0, 0, 1, 0)
-        val GB: ARGB = ARGB(0, 0, 1, 1)
         val B: ARGB = ARGB(0, 0, 0, 1)
 
         override def plus(x: ARGB, y: ARGB): ARGB = ARGB(x.a + y.a, x.r + y.r, x.g + y.g, x.b + y.b)
@@ -65,15 +39,15 @@ package object steganograph {
 
         override def fromInt(x: Int): ARGB = ARGB(1, x, x, x)
 
-        override def toInt(x: ARGB): Int = x.toARGB32
+        override def toInt(x: ARGB): Int = composeChannels(x.a, x.r, x.g, x.b)
 
-        override def toLong(x: ARGB): Long = x.toARGB32
+        override def toLong(x: ARGB): Long = toInt(x)
 
-        override def toFloat(x: ARGB): Float = x.toARGB32
+        override def toFloat(x: ARGB): Float = toInt(x)
 
-        override def toDouble(x: ARGB): Double = x.toARGB32
+        override def toDouble(x: ARGB): Double = toInt(x)
 
-        override def compare(x: ARGB, y: ARGB): Int = x.toARGB32 - y.toARGB32
+        override def compare(x: ARGB, y: ARGB): Int = zero.distanceSquared(x).compareTo(zero.distanceSquared(y))
 
         override def abs(x: ARGB): ARGB = ARGB(math.abs(x.a), math.abs(x.r), math.abs(x.g), math.abs(x.b))
 
